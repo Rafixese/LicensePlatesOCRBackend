196c1903e14961d4de8e2165a8aea9e20ced96b7
ovecka <owcaaa243@gmail.com>
2022-05-21 14:33:26 +0200

    Database

---
+++ database_init.py
@@ -0,0 +1,36 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from src.utils.solutionutils import get_project_root
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(get_project_root(), 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class LicensePlate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    plate_nb = db.Column(db.String(80), unique=True, nullable=False)
+    user_id = db.Column(db.Integer,  db.ForeignKey('user.id'), nullable=False )
+    comment = db.Column(db.String(80), nullable=False)
+
+
+    def __repr__(self):
+        return '<Plate %r>' % self.plate_nb
+
+
+db.create_all()
+db.session.commit()
