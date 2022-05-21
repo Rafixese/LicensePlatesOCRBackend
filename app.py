eeab09c4e1f88aba019f0d6812fac3a448906f65
ovecka <owcaaa243@gmail.com>
2022-05-21 18:20:08 +0200

    Login

--- app.py
+++ app.py
@@ -3,8 +3,10 @@
 import json
 import os
 
-from flask import Flask, Response
+from flask import Flask, Response, Blueprint, render_template, redirect, url_for, request
+from flask_login import UserMixin, LoginManager, login_user, login_required
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from src.utils.db_utils import get_all_plates
 from src.utils.license_plates_comparator import LicensePlatesComparator
@@ -40,12 +42,21 @@ class LicensePlate(db.Model):
 db.create_all()
 db.session.commit()
 
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
 
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call 
 # the associated function.
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
+# @login_required
 def hello_world():
     return 'Hello World'
 
@@ -62,6 +73,48 @@ def get_info_about_plate(plate_nb: str):
 
     return Response(status=404)
 
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login_reg.html')
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    # login code goes here
+    username = request.form.get('username')
+    password = request.form.get('password')
+    # remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(username=username).first()
+
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        return redirect(url_for('/login')) # if the user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user)
+    return redirect(url_for('/home'))
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    # code to validate and add user to database goes here
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(username=username).first() # if this returns a user, then the email already exists in database
+
+    if user: # if a user is found, we want to redir`ect back to signup page so user can try again
+        return redirect(url_for('signup'))
+
+    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+    new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('/login'))
 
 # main driver function
 if __name__ == '__main__':
